11e56260f3d9babbaf110dffd65c24cf6b033033
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2021-12-24 02:04:09 +0600

    input c file using gui notepad added

---
+++ compile.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+
+{
+    FILE *p1, *p2;
+    char c;
+    //okfffffffffffffffffffffffff//kfnkfn
+    /*
+        hhmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    */
+    p1=fopen(argv[2], "r");
+    p2=fopen("filtered.txt","w");
+    if(!p1)
+    {
+        printf("File cann't be opened!\n");
+    }
+    else
+    {
+        while((c=fgetc(p1))!=EOF)
+        {
+            if(c=='/')
+            {
+                c=fgetc(p1);
+                if(c=='/')
+                {
+                    while(c=fgetc(p1)!='\n')
+                    {
+
+                    }
+                }
+                else if(c=='*')
+                {
+                    while(c=fgetc(p1))
+                    {
+                        if(c=='*')
+                        {
+                            char d=fgetc(p1);
+                            if(d=='/')
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if(c!='\n')
+            {
+                fputc(c,p2);
+            }
+        }
+        fclose(p1);
+        fclose(p2);
+
+        p1=fopen("filtered.txt","r");
+        while((c=fgetc(p1))!=EOF)
+        {
+            printf("%c",c);
+        }
+
+        fclose(p1);
+    }
+}
+
+
+
--- compiler.py
+++ compiler.py
@@ -6,13 +6,13 @@ from tkinter.font import Font
 from tkinter.messagebox import *
 import time
 from subprocess import Popen, PIPE
+import tkinter.scrolledtext as scrolledtext
 
 
 image1=''
 main = Tk()
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cwd = os.getcwd()
-print(cwd)
 os.chdir(dir_path)
 main.bold_font = Font(family="Helvetica", size=14, weight="bold")
 main.title("Compiler")
@@ -37,28 +37,84 @@ def executeme():
     process = Popen("cmd.exe", shell=False, universal_newlines=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
     out, err = process.communicate(commands)
-    Label(main, text = str(out), font=('Impact', -15),borderwidth=1, relief="raised", fg='#000').grid(column= 0, row = 1)
-    print(out)
+    f = open(f"filtered.txt", "r")
+    Label(main, text = str(f.read()), font=('Impact', -15),borderwidth=1, relief="raised", fg='#000').grid(column= 0, row = 1)
+    f.close()
 
-def signinme():
-    third_face_recognition.testme()
+
+#get text for inserting note
+def get_text(*values):
+    if len(values[0])>0 and len(values[1])>0:
+        #print(str(values[1])+"' '"+str(values[0]))
+        f = open(f"{values[1]}", "w")
+        f.write(f"{values[0]}")
+        f.close()
+        commands = f'''cd {dir_path}
+        set Path=C:\\MinGW\\bin;%PATH%
+        gcc -o 180104042.exe 180104042.c
+        "{dir_path}\\180104042.exe" 2 "{values[1]}"
+        ''' #you can use " " for 1 line of commands or '''  ''' for several lines
+            #Single quotation marks won't do in that case. You have to add quotation marks around each path and also enclose the whole command in quotation marks:
+        process = Popen("cmd.exe", shell=False, universal_newlines=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        out, err = process.communicate(commands)
+        f = open(f"filtered.txt", "r")
+        alert=Tk()
+        alert.title('Output')
+        alert.minsize(1300, 700)
+        alert.maxsize(1300, 700)
+        alert.configure(background='#456')
+        Label(alert, text = str(f.read()),font=('Impact', -20),bg='#456',fg="#42ba96").place(relx = 0.5,
+                           rely = 0.5,
+                           anchor = 'center')
+        f.close()
+
+    else:
+        alert=Tk()
+        alert.title('Alert')
+        alert.minsize(400, 50)
+        alert.maxsize(400, 50)
+        alert.configure(background='#fff')
+        Label(alert, text = "Nothing to add due to empty!",bg="#fff",font=('Impact', -20),fg="#df4759").place(relx = 0.5,
+                           rely = 0.5,
+                           anchor = 'center')
+
+
+def compileme():
+    add=Tk()
+    add.title('Add a new note')
+    sub = Entry(add,font = ('courier', 15, 'bold'), width=50,foreground = 'green',borderwidth=15, relief=tk.SUNKEN)
+    sub.insert(0, "")
+    sub.pack(side=TOP, anchor=NW,expand=True, fill='both')
+
+    txt = scrolledtext.ScrolledText(add, undo=True)
+    txt['font'] = ('consolas', '12')
+    txt.pack(expand=True, fill='both')
+    txt.config(font=("consolas", 12), undo=True, wrap='word')
+    txt.config(borderwidth=5, relief="sunken")
+    add.showoriginal = tk.Button(add,width=15, text="Insert",font=('Impact', -20),fg='#fff', command= lambda:[get_text(txt.get('1.0', 'end-1c'),str(sub.get()))])
+    add.showoriginal.configure(background='#5cb85c')
+    add.showoriginal.pack()
+    add.showoriginal1 = tk.Button(add,width=15, text="Add Images",font=('Impact', -20),fg='#fff', command= lambda:[add_images(values[0])])
+    add.showoriginal1.configure(background='#0275d8')
+    add.showoriginal1.pack()
 
 
 
 def execute():
-        main.showoriginal = Button(main, text = "Execute",command = executeme)
+        main.showoriginal = Button(main, text = "Select",command = executeme)
         main.showoriginal.configure(background='#df4759',font=('Impact', -20),fg='#fff')
         main.showoriginal.place(relx = 0.37,
                            rely = 0.5,
                            anchor = 'center')
 
-def signin():
-        main.showoriginal = Button(main, text = "Sign-in",command = signinme)
+def compile():
+        main.showoriginal = Button(main, text = "Compile",command = compileme)
         main.showoriginal.configure(background='#42ba96',font=('Impact', -20),fg='#fff')
         main.showoriginal.place(relx = 0.63,
                            rely = 0.5,
                            anchor = 'center')
 
 execute()
-signin()
+compile()
 main.mainloop()
